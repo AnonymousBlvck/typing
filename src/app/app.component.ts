c056b34a9e08fc06e5ce46894c13c903b9a656c6
Andries <nando.chessplayer@gmail.com>
2022-10-27 15:11:48 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(event: any) {
+    this.enteredText = event.target.value;
+  }
+
+  compare(letter: string, enteredTextAtI: string) {
+    if (!enteredTextAtI) {
+      return 'pending';
+    } else if (letter === enteredTextAtI) {
+      return 'correct';
+    } else {
+      return 'incorrect';
+    }
+  }
 }
